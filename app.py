b461db45dbc92e2ff1fbf61260b2884d5ce1174b
Neel317 <neeldudheliya317@gmail.com>
2023-12-13 13:31:44 -0500

    Added templates and static files to serve using server.

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def home_page():
+    return render_template('index.html')
+
+@app.route('/forms')
+def form_page():
+    return render_template('forms.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
